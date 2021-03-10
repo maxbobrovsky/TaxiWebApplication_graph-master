83872bc045174877f7cec629b61bf226df7ffc8b
А А <professor.bobrr@gmail.com>
2021-03-10 02:43:54 +0200

    hello

---
+++ TaxiWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TaxiWebApplication.Models;
+using TaxiWebApplication.ViewModels;
+
+namespace TaxiWebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [Authorize(Roles = "admin,user")]
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _rolle;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<IdentityRole> rolle)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _rolle = rolle;
+        }
+
+        [HttpGet]
+        public IActionResult RegisterPage()
+        {
+            // var roles = new List<string> { "user", "driver" };
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem() { Text = "user", Value = "user", Selected = false });
+            items.Add(new SelectListItem() { Text = "driver", Value = "driver", Selected = true });
+
+            SelectList sl = new SelectList(items, "Value", "Text");
+            ViewData["Roles"] = sl;
+            var io = (List<SelectListItem>)sl.Items;
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterPage(RegisterViewModel model)
+            {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email, NativeCity = model.NativeCity };
+                // добавляем пользователя
+                var userRole = new IdentityRole(model.Role);
+                await _rolle.CreateAsync(userRole);
+                var result = await _userManager.CreateAsync(user, model.Password);
+                var creator = await _userManager.AddToRoleAsync(user, model.Role);
+                
+                if (result.Succeeded && creator.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult LoginPage(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public async Task<IActionResult> LoginPage(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    //if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    //{
+                    //    return Redirect(model.ReturnUrl);
+                    //}
+                    //else
+                    //{
+                        return RedirectToAction("Index", "Account");
+                    //}
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect login and (or) password");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ TaxiWebApplication/Controllers/OrderController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaxiWebApplication.Models;
+using TaxiWebApplication.Services;
+using TaxiWebApplication.ViewModels;
+
+namespace TaxiWebApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationContext _adb;
+        private IUserManagerService _userService;
+        private readonly KnnService _knn;
+        private readonly GraphRoadService _grs;
+        private readonly UserManager<User> _userManager;
+        //  private readonly UserManagerService _userService;
+
+        public OrderController(ApplicationContext adb, IUserManagerService userService, UserManager<User> userManager,
+            KnnService knn, GraphRoadService grs)
+        {
+            _adb = adb;
+            _userManager = userManager;
+            _userService = userService;
+            _knn = knn;
+            _grs = grs;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new OrderViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(OrderViewModel orderViewModel)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            int t = _knn.Parse(orderViewModel.Address);
+            string[] mas = orderViewModel.Address.Split(' ');
+            mas[0] = mas[0].Replace('.', ',');
+            mas[1] = mas[1].Replace('.', ',');
+            double[] unknown = new double[2];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                unknown[i] = double.Parse(mas[i]);
+            }
+
+
+            //if (_adb.Orders.Any(ord => ord.UserId == order.UserId))
+            //{
+            //    ModelState.AddModelError("Address", "Too much orders by for current user");
+            //    return View(ordModel);
+            //}
+
+            //_adb.Orders.Add(order);
+            //await _adb.SaveChangesAsync();
+
+
+            //double[] destination = new double[] { 50.644, 29.91 };
+
+            //double dist = _knn.DistanceToDest(unknown, destination);
+
+            ViewData["Distance"] = unknown[0];
+            ViewData["Price"] = unknown[1];
+
+            return RedirectToAction("DistanceAndPrice", "Order", new {distance = unknown[0], price = unknown[1]});
+
+           // return Json(new { redirectToUrl = Url.Action("DistanceAndPrice", "Order") });
+
+
+        }
+
+        public IActionResult DistanceAndPrice(decimal distance, decimal price)
+        {
+            ViewData["Distance"] = distance;
+            ViewData["Price"] = price;
+            return View();
+        }
+
+        [HttpPost]
+       
+        public async Task<JsonResult> VVDistance([FromBody]TwoMarkersViewModel viewModel)
+        {
+            TwoMarkersViewModel coords = new TwoMarkersViewModel
+            {
+                FirstLat = viewModel.FirstLat,
+                FirstLong = viewModel.FirstLong,
+                SecondLat = viewModel.SecondLat,
+                SecondLong = viewModel.SecondLong
+            };
+
+            double dist = await _grs.GraphDistance(coords.FirstLat, coords.FirstLong, coords.SecondLat, coords.SecondLong);
+            DistPriceViewModel mod = new DistPriceViewModel { Distance = dist, Price = dist * 6 };
+            
+
+            return new JsonResult(mod);
+        }
+
+    }
+}
